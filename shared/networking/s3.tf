b35f9433d5a5697e1d540411aa97a6a1cc9199c3
ihor.latyntsev <ihor.latyntsev@techtellent.io>
2024-04-02 13:35:32 +0300

    upd

--- shared/networking/s3.tf
+++ shared/networking/s3.tf
@@ -3,5 +3,31 @@ data "aws_caller_identity" "current" {}
 module "s3" {
   source = "../../modules/s3"
 
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+
   bucket_name = "${var.env}-s3-commonpurpose-${var.region}-${data.aws_caller_identity.current.account_id}"
+
+  attach_policy = true
+  user_provided_policies = [data.aws_iam_policy_document.static.json]
+}
+
+data "aws_iam_policy_document" "static" {
+
+  statement {
+    principals {
+      type        = "*"
+      identifiers = ["*"]
+    }
+    effect = "Allow"
+    actions = [
+      "s3:GetObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${var.env}-s3-commonpurpose-${var.region}-${data.aws_caller_identity.current.account_id}/files/*"
+    ]
+  }
 }
