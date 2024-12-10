fb3f135845aa483dea461042780baa5b3e6cb947
Latyn4ik <ilatincev@gmail.com>
2024-12-11 01:33:25 +0200

    /

--- dev/backend.tf
+++ dev/backend.tf
@@ -5,27 +5,3 @@ terraform {
     region = "us-east-1"
   }
 }
-
-
-
-
-aws elbv2 modify-rule \
-    --region <region> \
-    --rule-arn <rule-arn> \
-    --actions '[
-        {
-            "Type": "forward",
-            "ForwardConfig": {
-                "TargetGroups": [
-                    {
-                        "TargetGroupArn": "<backend-target-group-arn>",
-                        "Weight": 50
-                    },
-                    {
-                        "TargetGroupArn": "<backend-blue-target-group-arn>",
-                        "Weight": 50
-                    }
-                ]
-            }
-        }
-    ]'
