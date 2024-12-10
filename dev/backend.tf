858b8495171d2d9de3070899620c0000c1fdd7b6
ihor.latyntsev <ihor.latyntsev@techtellent.io>
2024-12-11 00:55:53 +0200

    /

--- dev/backend.tf
+++ dev/backend.tf
@@ -5,3 +5,27 @@ terraform {
     region = "us-east-1"
   }
 }
+
+
+
+
+aws elbv2 modify-rule \
+    --region <region> \
+    --rule-arn <rule-arn> \
+    --actions '[
+        {
+            "Type": "forward",
+            "ForwardConfig": {
+                "TargetGroups": [
+                    {
+                        "TargetGroupArn": "<backend-target-group-arn>",
+                        "Weight": 50
+                    },
+                    {
+                        "TargetGroupArn": "<backend-blue-target-group-arn>",
+                        "Weight": 50
+                    }
+                ]
+            }
+        }
+    ]'
